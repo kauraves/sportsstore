cc34e6e92ce36879037f2839d121818740b67085
Kauranen, Vesa <kauranen.vesa@gmail.com>
2020-05-01 21:53:33 +0300

    Chapter 06

--- data.js
+++ data.js
@@ -1,3 +1,27 @@
+var faker = require("faker");
+var data = [];
+var categories = ["Watersports", "Soccer", "Chess", "Running"];
+faker.seed(100);
+for (let i = 1; i <= 503; i++) {
+    var category = faker.helpers.randomize(categories);
+    data.push({
+        id: i,
+        name: faker.commerce.productName(),
+        category: category,
+        description: `${category}: ${faker.lorem.sentence(3)}`,
+        price: Number(faker.commerce.price())
+    })
+}
+
+module.exports = function () {
+    return {
+        categories: categories,        
+        products: data,
+        orders: []
+    }
+}
+
+/*
 module.exports = function () {
     return {
         categories: ["Watersports", "Soccer", "Chess"],
@@ -26,6 +50,7 @@ module.exports = function () {
         orders: []
     }
 }
+*/
 
 /*
 export const data = {
